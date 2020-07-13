706eed1ecb85653d6a64c1b884575945bd30b7f0
Yohann Marillet <yohann.marillet@gmail.com>
2020-07-13 09:31:12 +0200

    template is forced in parameter typecheck - no need to check the value in the method's body

--- src/TemplateManager.php
+++ src/TemplateManager.php
@@ -7,10 +7,6 @@ class TemplateManager
      */
     public function getTemplateComputed(Template $tpl, array $data)
     {
-        if (!$tpl) {
-            throw new \RuntimeException('no tpl given');
-        }
-
         $replaced = clone($tpl);
         $replaced->subject = $this->computeText($replaced->subject, $data);
         $replaced->content = $this->computeText($replaced->content, $data);
